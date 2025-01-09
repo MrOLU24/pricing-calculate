b74a5ef65e838d8a4af9891e42fc9f01d5865da2
Olatunji <abdulafeezoluwashola2@gmail.com>
2025-01-09 12:40:51 +0100

    updating the dom

---
+++ script.js
@@ -0,0 +1,13 @@
+function pricing() {
+  let val = document.getElementById("value").value;
+  let settleEl = document.getElementById("settle");
+  let feesEl = document.getElementById("fees");
+
+  //calculate
+  let charge = (1.5 / 100) * val;
+  let fees = val - charge;
+
+  //update the dom
+  settleEl.innerHTML = fees.toLocaleString();
+  feesEl.innerHTML = charge.toLocaleString();
+}
